9084c263fd2c9d0c6efc3f5e3542f873a2dda1f2
JAldo1980 <jamesalderman1980@gmail.com>
2024-02-06 10:57:01 +0000

    parameter update

---
+++ rest-parameter/index.js
@@ -0,0 +1,26 @@
+function getLabelsHtml(text, sender, ...staffObjs) {
+  const labelsHtml = staffObjs
+    .map(
+      (staffObj) =>
+        `<div class="label-card">
+    <p>Dear ${staffObj.name}</p>
+    <p>${text}</p>
+    <p>Best wishes,</p>
+    <p>${sender}</p>
+</div>`
+    )
+    .join("");
+  return labelsHtml;
+}
+
+const text = "Thank you for all your hard work throughout the year! 🙏🎁";
+const sender = "Tom";
+
+document.getElementById("labels-container").innerHTML = getLabelsHtml(
+  text,
+  sender,
+  { name: "Sally" },
+  { name: "Mike" },
+  { name: "Rob" },
+  { name: "Harriet" }
+);
