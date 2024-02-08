07e93c8b4c970cb51530db96163742ad7203be7b
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2024-02-08 10:51:05 +0000

    Add files via upload
    Using Constructor Functions to create custom Date() parameters

---
+++ constructor-date/index.js
@@ -0,0 +1,5 @@
+// A simple demonstration of using a contructor to create a new custom date.
+
+const dateSnapshot = new Date();
+
+console.log(`Copyright ${dateSnapshot.getFullYear().toString()}`);
